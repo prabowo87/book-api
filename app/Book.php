1afd0882eb15694e8370d3005dd47958786f847a
Satya Kresna <satyakresna6295@gmail.com>
2018-05-01 01:14:20 +0000

    Setup fillable and accessor get cover attribute

--- app/Book.php
+++ app/Book.php
@@ -3,8 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Book extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'cover'
+    ];
+
+    /**
+     * Get the user's first name.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getCoverAttribute($value)
+    {
+        return url("books/{$value}");
+    }
 }
