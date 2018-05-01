cffc846c3b94519daeee3f3dfc60ba1a535ede6d
Satya Kresna <satyakresna6295@gmail.com>
2018-05-01 01:14:49 +0000

    Set cover as nullable

--- database/migrations/2018_04_30_231645_create_books_table.php
+++ database/migrations/2018_04_30_231645_create_books_table.php
@@ -16,7 +16,7 @@ class CreateBooksTable extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->string('cover');
+            $table->string('cover')->nullable();
             $table->timestamps();
         });
     }
