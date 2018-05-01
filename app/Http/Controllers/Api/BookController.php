30a106ab6bdc11b6b59117f30a72abe0cf5fe0e2
Satya Kresna <satyakresna6295@gmail.com>
2018-05-01 01:15:15 +0000

    Implement store and update book controller

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Book;
+use Validator;
+use Illuminate\Support\Facades\Storage;
 
 class BookController extends ApiController
 {
@@ -25,7 +27,23 @@ class BookController extends ApiController
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string',
+            'cover' => 'required|image',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->respondError($validator->errors(), 422);
+        }
+
+        $book = new Book;
+
+        $book->title = $request->input('title');
+        $book->cover = $request->cover->store('', 'books');
+
+        $book->save();
+
+        return $book->paginate();
     }
 
     /**
@@ -54,12 +72,32 @@ class BookController extends ApiController
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  Book $book
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Book $book)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string',
+            'cover' => 'image',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->respondError($validator->errors(), 422);
+        }
+
+        if ($request->has('title')) {
+            $book->title = $request->input('title');
+        }
+
+        if ($request->hasFile('cover')) {
+            Storage::delete($book->cover);
+            $book->cover = $request->cover->store('', 'books');
+        }
+
+        $book->save();
+
+        return $book->paginate();
     }
 
     /**
